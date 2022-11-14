081554f007fe8173939822e6ec3dbed2c6f4912c
GlavS <pianoforte2@yandex.ru>
2022-11-14 23:42:10 +0300

    Start modified as Singleton

--- P2-FactoryMethod/src/Start.java
+++ P2-FactoryMethod/src/Start.java
@@ -1,15 +1,14 @@
 public class Start {
     public static void main(String[] args) {
 
-        CarSelector factory = CarSelector.getInstance();
 
-        Car car = factory.getCar(RoadType.HIGHWAY);
+        Car car = CarSelector.getInstance().getCar(RoadType.HIGHWAY);
         car.drive();
         car.stop();
-        car = factory.getCar(RoadType.OFFROAD);
+        car = CarSelector.getInstance().getCar(RoadType.OFFROAD);
         car.drive();
         car.stop();
-        car = factory.getCar(RoadType.HIGHWAY);
+        car = CarSelector.getInstance().getCar(RoadType.HIGHWAY);
         car.drive();
         car.stop();
 
