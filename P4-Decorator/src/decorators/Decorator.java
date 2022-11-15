d3e121fecf297e2a1691d455f891188573a73c14
GlavS <pianoforte2@yandex.ru>
2022-11-15 16:49:41 +0300

    Classes added to Decorator sample

--- P4-Decorator/src/decorators/Decorator.java
+++ P4-Decorator/src/decorators/Decorator.java
@@ -1,5 +1,21 @@
 package decorators;
 
-public abstract class Decorator {
+import objects.Component;
 
+//можно и интерфейс. Наследуем от родительского интерфейса декорируемых объектов
+public abstract class Decorator implements Component {
+
+    protected Component component;
+
+    public Decorator(Component component) {
+        this.component = component;
+    }
+
+    public abstract void afterDraw();
+
+    @Override
+    public void draw() {
+        component.draw();
+        afterDraw();
+    }
 }
