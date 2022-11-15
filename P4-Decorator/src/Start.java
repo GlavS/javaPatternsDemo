1661528cc15d24289bc902d2c95a7534ce1c8d2a
GlavS <pianoforte2@yandex.ru>
2022-11-15 17:53:33 +0300

    Added Button class, edited Start

--- P4-Decorator/src/Start.java
+++ P4-Decorator/src/Start.java
@@ -1,13 +1,36 @@
 import decorators.BorderDecorator;
 import decorators.ColorDecorator;
+import objects.Button;
 import objects.Component;
+import objects.TextView;
 import objects.Window;
 
 public class Start {
+    private static Component window;
+    private static Component textView;
+    private static Component button;
+
     public static void main(String[] args) {
         // Декораторы можно вкладывать друг в друга
+
         Component borderedWindow = new ColorDecorator(new BorderDecorator(new Window()));
         borderedWindow.draw();
+        // Декорирование по условию
+        // Очень легко добавлять новые объекты (добавим компонент Button)
+        boolean needBorder = true; // Флаг
+        if (needBorder) {
+            window = new BorderDecorator(new Window());
+            textView = new BorderDecorator(new TextView());
+            button = new BorderDecorator(new Button());
+        } else {
+            window = new Window();
+            textView = new TextView();
+            button = new Button();
+        }
+
+        window.draw();
+        textView.draw();
+        button.draw();
 
     }
 }
---
+++ P4-Decorator/src/objects/Button.java
@@ -0,0 +1,8 @@
+package objects;
+
+public class Button implements Component{
+    @Override
+    public void draw() {
+        System.out.println("Button displayed");
+    }
+}
